1a95aa6799dbe78bd978e404306738034d53cb60
= <zainr7989@gmail.com>
2020-02-02 18:52:08 -0800

    Add test for home page route.

--- tests.py
+++ tests.py
@@ -11,6 +11,11 @@ class ZankTests(TestCase):
         # show errors from Flask that are raised in testing
         app.config['TESTING'] = True
 
+    def test_home_page(self):
+        '''User is able to get the home page, and see intro info about Zank.'''
+        result = self.client.get('/')
+        self.assertEqual(result.status, '200 OK')
+
 
 if __name__ == '__main__':
     unittest_main()
