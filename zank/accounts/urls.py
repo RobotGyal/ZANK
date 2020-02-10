ba9d8ca312069913c4b67aa578674f78be323f05
= <zainr7989@gmail.com>
2020-02-09 22:03:22 -0800

    Add url modules in apps.

---
+++ zank/accounts/urls.py
@@ -0,0 +1,3 @@
+
+# include a url pattern for the login Page
+# include a url pattewrn for the signup page
---
+++ zank/codes/urls.py
@@ -0,0 +1,5 @@
+
+# include a url pattern for the home Page
+# inlucde one for the results/reference page (the list view)
+# inlcude one for the details of one code
+# include one for the create, update, and deletion of a page
