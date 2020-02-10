d7572079f1a89455b543a233badd92ca172dd17c
= <zainr7989@gmail.com>
2020-02-09 19:20:50 -0800

    Refactor auth validators for readability.

--- zank/zank/settings.py
+++ zank/zank/settings.py
@@ -98,19 +98,23 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
+VALIDATOR_1 = (
+    'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'
+)
+VALIDATOR_2 = (
+    'django.contrib.auth.password_validation.MinimumLengthValidator'
+)
+VALIDATOR_3 = (
+    'django.contrib.auth.password_validation.CommonPasswordValidator'
+)
+VALIDATOR_4 = (
+    'django.contrib.auth.password_validation.NumericPasswordValidator'
+)
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {'NAME': VALIDATOR_1},
+    {'NAME': VALIDATOR_2},
+    {'NAME': VALIDATOR_3},
+    {'NAME': VALIDATOR_4},
 ]
 
 
