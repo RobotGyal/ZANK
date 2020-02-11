76ff2cf175bcf1802212a79169aefbacf6c0235e
= <zainr7989@gmail.com>
2020-02-11 12:17:47 -0800

    Declare SignUpView.

--- zank/accounts/views.py
+++ zank/accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from accounts.forms import SignUpForm
+from .models import ArchitectOrOfficer
+from django.urls import reverse, reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.models import User
+from django.contrib.auth import views as auth_views
 
-# Create your views here.
+
+class SignUpView(SuccessMessageMixin, CreateView):
+    '''Allows site visitors to set up a new account as architect or officer.'''
+    pass
