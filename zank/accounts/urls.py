e91c57218294297e959c3b1c710da98a0ab75b16
= <zainr7989@gmail.com>
2020-02-17 10:02:57 -0800

    Uncomment url path for login.

--- zank/accounts/urls.py
+++ zank/accounts/urls.py
@@ -11,9 +11,7 @@ app_name = 'accounts'
 
 urlpatterns = [
     path('signup/', SignUpView.as_view(), name="signup"),
-    """
     path('login/',
          auth_views.LoginView.as_view(template_name="accounts/login.html"),
          name='login'),
-    """
 ]
